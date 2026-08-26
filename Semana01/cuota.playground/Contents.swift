251e30857c4717a9797badc51995ce2e08c7faff
FavioSolorzano <favio.solorzano@tecsup.edu.pe>
2026-08-26 09:42:07 -0500

    Primer commit de prueba de practica

---
+++ Semana01/cuota.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// calendario de pagos
+//ingresar el nombre del producto, precio, cantidad
+let igv = 0.18
+func CalcularDescuento(cantidad: Int, subtotal: Double)->Double{
+    if cantidad > 10 {
+        return subtotal * 0.15
+    }else if cantidad > 5 {
+        return subtotal * 0.10
+    }else
+    {
+        return 0.0
+    }
+}
+print("ingrese el nombre del producto")
+let producto = readLine() ?? ""
+print("ingrese el precio del producto")
+let precio = Double(readLine() ?? "0") ?? 0.0
+print("ingrese la cantidad del producto")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+let subtotal = precio * Double(cantidad)
+let descuento = CalcularDescuento(cantidad: cantidad, subtotal: subtotal)
+//hallar el montobase : subtotal - descuento
+//hallar el igv : montobase * igv
+//total : montobase + igv
+//mostrar datos
