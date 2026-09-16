1c2e81b3f99d90006ca6029f7eefd26bc1e9446b
FavioSolorzano <favio.solorzano@tecsup.edu.pe>
2026-09-16 10:14:27 -0500

    feat: crear interfaz de calculadora de prestamos

---
+++ Semana05/Laboratorio-CP/Laboratorio-CP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Laboratorio-CP
+//
+//  Created by Tecsup on 16/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var capitalTextField: UITextField!
+    @IBOutlet weak var tasaTextField: UITextField!
+    @IBOutlet weak var plazoTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = "Ingrese los datos del préstamo"
+    }
+
+    @IBAction func calcularPrestamo(_ sender: Any) {
+        
+        
+    
+}
+
